1e83cd7ca7eeebd7f45361c997644c700644120a
Alok <karnaalok48@gmail.com>
2024-07-18 23:04:48 +0545

    root size

--- Task 2/main.py
+++ Task 2/main.py
@@ -1,5 +1,8 @@
 from tkinter import *
 
 root = Tk()
+root.geometry("400x425")
+root.resizable(False,False)
+root.title("Calculator project")
 root.iconbitmap("D:\CodSoft\Task 2\photos\download.ico") 
 root.mainloop()
