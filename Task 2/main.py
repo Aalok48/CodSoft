a494f23c6abc6220ba0ecc9ad331754af9f898b3
Alok <karnaalok48@gmail.com>
2024-07-19 22:42:59 +0545

    click function

--- Task 2/main.py
+++ Task 2/main.py
@@ -5,4 +5,24 @@ root.geometry("400x425")
 root.resizable(False,False)
 root.title("Calculator project")
 root.iconbitmap("D:\CodSoft\Task 2\photos\download.ico") 
+
+def click(event):
+    text=event.widget.cget("text")
+    if text=="=":
+        if scvalue.get().isdigit():
+          value = int(scvalue.get())
+        else:
+            value=eval(screen.get())
+        scvalue.set(value)
+        screen.update()
+        
+
+    elif text=="c":
+        scvalue.set("")
+        screen.update()
+
+    else:
+        scvalue.set(scvalue.get()+text)
+        screen.update()
+
 root.mainloop()
