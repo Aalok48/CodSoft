ff71bceb65e1c50996f56c5ab8c3d20ffab06f98
Alok <karnaalok48@gmail.com>
2024-07-21 16:49:17 +0545

    button frame and function created

--- Task 5/main.py
+++ Task 5/main.py
@@ -16,6 +16,41 @@ l1.pack(anchor="center")
 
 # frame for all the buttons
 button_frame = Frame(root, width=350, height=300)
-button_frame.place(x=35, y=95)
+button_frame.place(x=55, y=105)
+
+def add():
+    pass
+
+def delete():
+    pass
+
+def edit():
+    pass
+
+def view():
+    pass
+
+def search():
+    pass
+
+# creating first button named add a contact
+b1 = Button(button_frame, text="1. Add a contact", command=add, borderwidth=0, font="15")
+b1.pack(fill=X)
+
+# creating second button named delete a contact
+b2 = Button(button_frame, text="     2. Delete a Contact", command=delete, borderwidth=0, font=15)
+b2.pack(fill=X, pady=5)
+
+# creating third button named edit a contact
+b3 = Button(button_frame, text="3. Edit a contact", command=edit, borderwidth=0, font=15)
+b3.pack(pady=2)
+
+# creating forth button named see all contacts
+b4 = Button(button_frame, text="     4. View all contacts", borderwidth=0, command=view, font=15)
+b4.pack(pady=5)
+
+# creating fifth button named search contacts
+b5 = Button(button_frame, text="     5. Search a contact", command=search, borderwidth=0, font="15")
+b5.pack(pady=5)
 
 root.mainloop()
