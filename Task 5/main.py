09f2009db21a09c710776a134fa7f30f9bcda90f
Alok <karnaalok48@gmail.com>
2024-07-21 16:44:46 +0545

    root created

---
+++ Task 5/main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import messagebox as tmsg
+
+root = Tk()
+root.geometry("400x450")
+root.resizable(False, False)
+root.title("Contact Management System")
+root.iconbitmap("D:\CodSoft\Task 5\Photos\contact.ico")
+
+
+root.mainloop()
